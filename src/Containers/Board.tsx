fcc365d79fb7b13bc2f7849fc930d3d07a7af7fa
arkaaaadiy <42414769+arkaaaadiy@users.noreply.github.com>
2020-05-19 22:18:04 +0500

    Add rotate card when draggbel

--- src/Containers/Board.tsx
+++ src/Containers/Board.tsx
@@ -5,7 +5,37 @@ import AddNewItem from '../components/AddNewItem/AddNewItem';
 import Card from '../components/Card/Card';
 import { rootState } from '../Store/store';
 import { actions } from '../Store/actions/panelActions';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import {
+	DragDropContext,
+	Droppable,
+	Draggable,
+	DropResult,
+	DraggableStateSnapshot,
+	DraggingStyle,
+	NotDraggingStyle,
+} from 'react-beautiful-dnd';
+
+function getStyle(style: DraggingStyle | NotDraggingStyle, snapshot: DraggableStateSnapshot) {
+	
+	if (!snapshot.isDragging || snapshot.isDropAnimating) {
+		console.log(snapshot);
+		return style;
+	}	
+
+	const translate = style.transform;
+	// move to the right spot
+
+	// add a bit of turn for fun
+	const rotate = 'rotate(5deg)';
+
+	// patching the existing style
+	return {
+		...style,
+		transform: `${translate} ${rotate}`,
+		// transition: `all 0.001s`,
+		transitionDuration: `0.001s`
+	};
+}
 
 const Board = (props: Props) => {
 	const { columns = [], createColumn, createCard, reorderCards, moveCards } = props;
@@ -14,7 +44,7 @@ const Board = (props: Props) => {
 		(result: DropResult) => {
 			const { source, destination } = result;
 			if (destination === null) {
-				return
+				return;
 			}
 			source.droppableId === destination?.droppableId
 				? reorderCards(source, destination!)
@@ -44,6 +74,8 @@ const Board = (props: Props) => {
 															{...provided.dragHandleProps}
 															key={item.id}
 															title={item.title}
+															// onDeleteCard={}
+															style={getStyle(provided.draggableProps.style!, snapshot)}
 														/>
 													)}
 												</Draggable>
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import './Card.sass';
 
 interface CardProps {
 	title: string
 	innerRef?: any
 	className?: string
+	style: CSSProperties
 }
 
 const Card: React.FC<CardProps> = (props) => {
-	const {title, innerRef, className, ...restprops} = props
+	const {title, innerRef, className, style, ...restprops} = props
     return (
-		<div {...restprops} ref={innerRef} className={'card' + className}>
+		<div  {...restprops} ref={innerRef} style={style} className={'card' + className}>
+			<div className='icon-close card__delete'></div>
 			<span>
             {title}
 			</span>
